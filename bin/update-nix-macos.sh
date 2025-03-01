4da6299d47961c65c05783c607852629eb3f306d
Jessie Slight <jslight90@gmail.com>
2025-03-01 20:13:24 +0000

    bin/update: use git reset instead of git pull

--- bin/update-nix-macos.sh
+++ bin/update-nix-macos.sh
@@ -7,6 +7,6 @@ cd /var/lib/nix-macos
 nix-shell -p git --run "git fetch"
 changes=`nix-shell -p git --run "git rev-list HEAD..origin/main --count"`
 if [ $changes -ne 0 ]; then
-  nix-shell -p git --run "git pull --force"
+  nix-shell -p git --run "git reset --hard origin/main"
   darwin-rebuild switch --flake .#$architecture
 fi
