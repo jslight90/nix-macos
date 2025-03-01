9989579263f32f515e8f66eb4c4ec43726c6129e
Jessie Slight <jslight90@gmail.com>
2025-03-01 20:10:06 +0000

    bin/update: fix syntax for git command to variable

--- bin/update-nix-macos.sh
+++ bin/update-nix-macos.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 architecture=`uname -m`
 cd /var/lib/nix-macos
 nix-shell -p git --run "git fetch"
-changes=nix-shell -p git --run "git rev-list HEAD..origin/main --count"
+changes=`nix-shell -p git --run "git rev-list HEAD..origin/main --count"`
 if [ $changes -ne 0 ]; then
   nix-shell -p git --run "git pull --force"
   darwin-rebuild switch --flake .#$architecture
