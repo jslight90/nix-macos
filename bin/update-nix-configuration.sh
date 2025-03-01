651752108d35ffadf0f46db914b75ab5685ae8ca
Jessie Slight <jslight90@gmail.com>
2025-03-01 11:34:26 -0800

    bin/update: only pull and apply configuration if there are changes

--- bin/update-nix-configuration.sh
+++ bin/update-nix-configuration.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 cd /var/lib/nix-macos
-nix-shell -p git --run "git pull --force"
-darwin-rebuild switch --flake .#eo
+nix-shell -p git --run "git fetch"
+changes=nix-shell -p git --run "git rev-list HEAD..origin/main --count"
+if [ $changes -ne 0 ]; then
+  nix-shell -p git --run "git pull --force"
+  darwin-rebuild switch --flake .#eo
+fi
